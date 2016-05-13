e82611d866f3c4179b8f74a5764281736f257ad0
nikolas-x <nikolas@mastiha.net>
2016-05-13 10:11:03 +1000

    Completed Tutorial-10b

--- tests.cpp
+++ tests.cpp
@@ -25,7 +25,7 @@ void Tests::sortTest()
     vector.push_back(1);
     vector.push_back(2);
 
-    // need to sort the vector
+    std::sort(vector.begin(), vector.end());
 
     QVERIFY(1 == vector.at(0) && 2 == vector.at(1) && 3 == vector.at(2));
 }
@@ -42,7 +42,7 @@ void Tests::reverseSortTest()
     vector.push_back(1);
     vector.push_back(2);
 
-    // need to sort the vector in descending order
+    std::sort(vector.begin(), vector.end(), reverse);
 
     QVERIFY(3 == vector.at(0) && 2 == vector.at(1) && 1 == vector.at(2));
 }
@@ -58,8 +58,7 @@ void Tests::bogoSortTest()
     int timeout = 0;
     while(!std::is_sorted(vector.begin(), vector.end()) && timeout++ < 1000000)
     {
-        // shuffle the vector in a random order. If we keep trying we are bound
-        // to get it sorted right???
+        std::random_shuffle(vector.begin(), vector.end());
     }
 
     QVERIFY(timeout < 1000000);
@@ -76,8 +75,8 @@ void Tests::findTest()
 
     // find1 looks to see if there is a 2 in the list
     // and find2 should check if there is a 5 in the list
-    std::vector<int>::const_iterator find1 = vector.end();
-    std::vector<int>::const_iterator find2 = vector.end();
+    std::vector<int>::const_iterator find1 = std::find(vector.begin(), vector.end(), 2);
+    std::vector<int>::const_iterator find2 = std::find(vector.begin(), vector.end(), 5);
 
     QVERIFY(vector.end() != find1 && 2 == *find1 && vector.end() == find2);
 }
@@ -95,11 +94,11 @@ void Tests::replaceTest()
     vector.push_back(2);
     vector.push_back(4);
 
-	// need to update find
-    std::vector<int>::const_iterator find = vector.end();
+    std::vector<int>::iterator find = std::find(vector.begin(), vector.end(), 2);
     while (vector.end() != find)
     {
-        // repalce all 2s with 3s and then update find
+        *find = 3;
+        find = std::find(vector.begin(), vector.end(), 2);
     }
 
     int numberOfThrees = std::count(vector.begin(), vector.end(), 3);
